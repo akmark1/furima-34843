557b0349cf8ce32b242b3c844088e320d07c6889
akmark1 <akmark123@gmail.com>
2021-03-15 17:23:00 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
+  validates :first_name, format: { with: /\A[一-龥]+\z/ }, presence: true
+  validates :last_name, format: { with: /\A[一-龥]+\z/ }, presence: true
+  validates :first_name_read, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
+  validates :last_name_read, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
 end
