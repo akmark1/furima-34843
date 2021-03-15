ff52637f026f2afb5c60c082daa4cabe005b6050
akmark1 <akmark123@gmail.com>
2021-03-15 21:23:00 +0900

    ユーザー新規登録のテストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,13 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
     password_confirmation {password}
+    first_name {Gimei.name.first.kanji}
+    last_name {Gimei.name.last.kanji}
+    first_name_read {Gimei.name.first.katakana}
+    last_name_read {Gimei.name.last.katakana}
+    birthday {Faker::Time.backward}
   end
-end
+end 
