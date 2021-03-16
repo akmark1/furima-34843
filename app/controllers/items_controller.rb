880b17389c138080c462a976670139e086b8e8ba
akmark1 <akmark123@gmail.com>
2021-03-16 15:44:42 +0900

    Active strageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,13 @@ class ItemsController < ApplicationController
   def new
     @items = Item.new
   end
+  
+  def create
+  end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_id, :state_id, :delivery_day_id, :value).merge(user_id: current_user.id)
+  end
 end
