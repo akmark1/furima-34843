63008ab9cd5fbce6b6046e8c08ca2385d362100f
akmark1 <akmark123@gmail.com>
2021-03-17 19:45:04 +0900

    投稿者かログインしているかでページの偏移を調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: [:edit]
 
   def index 
     @items = Item.order("created_at DESC")
@@ -41,4 +42,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :delivery_id, :state_id, :delivery_day_id, :value).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
 end
