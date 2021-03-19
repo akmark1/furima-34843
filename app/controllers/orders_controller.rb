d2d11a0dd7b1c8662b0ed4f15cb2578be9761667
akmark1 <akmark123@gmail.com>
2021-03-19 14:42:31 +0900

    不要な記述の削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,2 @@
 class OrdersController < ApplicationController
-
-  def index
-  end
-
-  def new
-  end
-
-  def create
-    @order = Order.create(order_params)
-
-  end
-  
-  private
-
-  def order_params
-    params.permit(:zip_code, :state_id, :city, :house_number, :room_number, :phone_number).merge(buy_id: @buy.id)
-  end
-
 end
