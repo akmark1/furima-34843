26f61e1790669e1891bba46fb3313b49db9ea4dc
akmark1 <akmark123@gmail.com>
2021-03-19 16:32:15 +0900

    テストコードを異常系、正常系に分岐。テストコード追加

--- spec/models/buy_order_spec.rb
+++ spec/models/buy_order_spec.rb
@@ -9,61 +9,75 @@ RSpec.describe BuyOrder, type: :model do
       sleep 0.1
     end
 
-    it 'すべての値が正しく入力されていれば保存できること' do
-      expect(@buy_order).to be_valid
+    context '商品の購入ができる時' do
+
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@buy_order).to be_valid
+      end
+
+      it 'room_numberは空でも保存できること' do
+        @buy_order.room_number = ''
+        expect(@buy_order).to be_valid
+      end
+
     end
 
-    it 'zip_codeが空だと保存できないこと' do
-      @buy_order.zip_code = ''
-      @buy_order.valid?
-      expect(@buy_order.errors.full_messages).to include("Zip code can't be blank")
-    end
+    context '商品の購入ができない時' do
 
-    it 'zip_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
-      @buy_order.zip_code = '1234567'
-      @buy_order.valid?
-      expect(@buy_order.errors.full_messages).to include('Zip code is invalid. Include hyphen(-)')
-    end
+      it 'zip_codeが空だと保存できないこと' do
+        @buy_order.zip_code = ''
+        @buy_order.valid?
+        expect(@buy_order.errors.full_messages).to include("Zip code can't be blank")
+      end
+
+      it 'zip_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @buy_order.zip_code = '1234567'
+        @buy_order.valid?
+        expect(@buy_order.errors.full_messages).to include('Zip code is invalid. Include hyphen(-)')
+      end
     
-    it 'stateが空だと保存できないこと' do
-      @buy_order.state = nil
-      @buy_order.valid?
-      expect(@buy_order.errors.full_messages).to include("State is not a number")
-    end
+      it 'stateが空だと保存できないこと' do
+        @buy_order.state = nil
+        @buy_order.valid?
+        expect(@buy_order.errors.full_messages).to include("State is not a number")
+      end
 
-    it "stateが1では保存できないこと" do
-      @buy_order.state = 1
-      @buy_order.valid?
-      expect(@buy_order.errors.full_messages).to include("State must be other than 1")
-    end
+      it "stateが1では保存できないこと" do
+        @buy_order.state = 1
+        @buy_order.valid?
+        expect(@buy_order.errors.full_messages).to include("State must be other than 1")
+      end
 
-    it 'cityが空だと保存できないこと' do
-      @buy_order.city = ''
-      @buy_order.valid?
-      expect(@buy_order.errors.full_messages).to include("City can't be blank")
-    end
+      it 'cityが空だと保存できないこと' do
+        @buy_order.city = ''
+        @buy_order.valid?
+        expect(@buy_order.errors.full_messages).to include("City can't be blank")
+      end
 
-    it 'house_numberが空だと保存できないこと' do
-      @buy_order.house_number = ''
-      @buy_order.valid?
-      expect(@buy_order.errors.full_messages).to include("House number can't be blank")
-    end
+      it 'house_numberが空だと保存できないこと' do
+        @buy_order.house_number = ''
+        @buy_order.valid?
+        expect(@buy_order.errors.full_messages).to include("House number can't be blank")
+      end
 
-    it 'room_numberは空でも保存できること' do
-      @buy_order.room_number = ''
-      expect(@buy_order).to be_valid
-    end
+      it 'phone_numberが空だと保存できないこと' do
+        @buy_order.phone_number = ''
+        @buy_order.valid?
+        expect(@buy_order.errors.full_messages).to include("Phone number can't be blank")
+      end
 
-    it 'phone_numberが空だと保存できないこと' do
-      @buy_order.phone_number = ''
-      @buy_order.valid?
-      expect(@buy_order.errors.full_messages).to include("Phone number can't be blank")
-    end
+      it 'phone_numberが11桁より多いと保存できないこと' do
+        @buy_order.phone_number = '000000000000'
+        @buy_order.valid?
+        expect(@buy_order.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
+      end
 
-    it 'phone_numberが11桁より多いと保存できないこと' do
-      @buy_order.phone_number = '000000000000'
-      @buy_order.valid?
-      expect(@buy_order.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
+      it 'phone_numberは英数混合では保存できないこと' do
+        @buy_order.phone_number = '080abcd1234'
+        @buy_order.valid?
+        expect(@buy_order.errors.full_messages).to include("Phone number is invalid")
+      end
+
     end
 
   end
